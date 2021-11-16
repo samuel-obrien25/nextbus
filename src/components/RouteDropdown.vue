3d06adeae5e846b828c41dee33a521a6875108d4
samuel-obrien25 <samuel.obrien25@gmail.com>
2021-11-16 15:59:18 -0500

    request user location on load

---
+++ src/components/RouteDropdown.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='RouteDropdown__Container' />
+</template>
+
+<script>
+
+export default {
+  name: 'RouteDropdown',
+  computed: {
+    busLinesArr: [
+      1, 2, 3,
+    ],
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scss scoped>
+</style>
